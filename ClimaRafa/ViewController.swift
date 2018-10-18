28a316d998dab11dfd6b775b9b75c5496edf838b
Alumno <ulsa@Obed-Miranda.local>
2018-10-18 13:50:01 -0700

    hola

--- ClimaRafa/ViewController.swift
+++ ClimaRafa/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblGrados: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlClima = "http://api.openweathermap.org/data/2.5/weather?q=Ciudad+Obregon&APPID=3326d4de85a204e001116973cc1e557a"
+        
+        Alamofire.request(urlClima).responseJSONit {
+            response in
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
